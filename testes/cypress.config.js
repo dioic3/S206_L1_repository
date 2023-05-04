6cd2f7e07e65eb69042aef652c0d296ed58ae84f
dioic3 <joyce.dias@gec.inatel.br>
2023-05-04 19:17:24 -0300

    adição do dotenv

--- testes/cypress.config.js
+++ testes/cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env.baseurl = process.env.BASE_URL
+      return config
     },
   },
 });
